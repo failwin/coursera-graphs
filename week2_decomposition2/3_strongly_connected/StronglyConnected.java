04cb8755d20196413bf722cbc52be2ae658bc512
Yurii Kovalchuk <yurii.kovalchuk@sisense.com>
2020-01-30 20:37:30 +0200

    week 2 StronglyConnected switch form tests

--- week2_decomposition2/3_strongly_connected/StronglyConnected.java
+++ week2_decomposition2/3_strongly_connected/StronglyConnected.java
@@ -194,27 +194,27 @@ public class StronglyConnected {
     }
 
     public static void main(String[] args) {
-        try {
-            test();
-            System.out.println("Tests passed");
-        } catch (Error err) {
-            System.out.println(err.getMessage());
-        }
+//        try {
+//            test();
+//            System.out.println("Tests passed");
+//        } catch (Error err) {
+//            System.out.println(err.getMessage());
+//        }
 
-//        Scanner scanner = new Scanner(System.in);
-//        int n = scanner.nextInt();
-//        int m = scanner.nextInt();
-//        ArrayList<Integer>[] adj = (ArrayList<Integer>[])new ArrayList[n];
-//        for (int i = 0; i < n; i++) {
-//            adj[i] = new ArrayList<Integer>();
-//        }
-//        for (int i = 0; i < m; i++) {
-//            int x, y;
-//            x = scanner.nextInt();
-//            y = scanner.nextInt();
-//            adj[x - 1].add(y - 1);
-//        }
-//        System.out.println(numberOfSCC(adj));
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        ArrayList<Integer>[] adj = (ArrayList<Integer>[])new ArrayList[n];
+        for (int i = 0; i < n; i++) {
+            adj[i] = new ArrayList<Integer>();
+        }
+        for (int i = 0; i < m; i++) {
+            int x, y;
+            x = scanner.nextInt();
+            y = scanner.nextInt();
+            adj[x - 1].add(y - 1);
+        }
+        System.out.println(numberOfSCC(adj));
     }
 
     public static void test() {
